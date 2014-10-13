c15154f07db74c060c724e8c8b89ae0964b33ed2
Rana Das <rana.pratap.das@gmail.com>
2014-10-13 17:34:55 +0100

    adding some sources

---
+++ SampleMain.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   SampleMain.cpp
+ * Author: rdas
+ *
+ * Created on 13 October 2014, 17:34
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=coderdojod15cpptutorial
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/coderdojod15cpptutorial
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=coderdojod15cpptutorial.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/coderdojod15cpptutorial.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=coderdojod15cpptutorial
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/coderdojod15cpptutorial
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=coderdojod15cpptutorial.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/coderdojod15cpptutorial.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
